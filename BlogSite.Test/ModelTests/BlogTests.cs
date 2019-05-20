7d127e674021a48118eceb3f0572526aa81b3be7
Zema <zebede15@gmail.com>
2019-05-20 14:51:20 -0700

    Rename view folders

--- BlogSite.Test/ModelTests/BlogTests.cs
+++ BlogSite.Test/ModelTests/BlogTests.cs
@@ -1,23 +1,64 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using BlogSite.Models;
-// using System.Collections.Generic;
-// using System;
-// using MySql.Data.MySqlClient;
-//
-// namespace BlogSite.Tests
-// {
-//   [TestClass]
-//   public class BlogTest
-//   {
-//     public BlogTest()
-//     {
-//       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=blog_site_test;";
-//     }
-//     [TestMethod]
-//     public void BlogConstructor_CreatesNewBlog_Blog()
-//     {
-//       Blog newBlog = new Blog
-//     }
-//   }
-//
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlogSite.Models;
+using System.Collections.Generic;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace BlogSite.Tests
+{
+  [TestClass]
+  public class BlogTest
+  {
+    public BlogTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=blog_site_test;";
+    }
+    [TestMethod]
+    public void BlogConstructor_CreatesNewBlog_Blog()
+    {
+      Blog newBlog = new Blog("", "");
+      Assert.AreEqual(typeof(Blog), newBlog.GetType());
+    }
+    [TestMethod]
+    public void GetUserName_GetsNameOfBlogUser_String()
+    {
+      string username =
+      Blog newBlog = new Blog(username, "");
+      Assert.AreEqual(username, newBlog.GetUserName());
+    }
+   [TestMethod]
+   public void GetPassword()
+   {
+
+   }
+   [TestMethod]
+   public void GetTitle()
+   {
+
+   }
+   [TestMethod]
+   public void SetTitle()
+   {
+
+   }
+   [TestMethod]
+   public void GetId()
+   {
+
+   }
+   [TestMethod]
+   public void GetAbout()
+   {
+
+   }
+   [TestMethod]
+   public void SetAbout()
+   {
+
+   }
+
+
+
+
+ }
+}
