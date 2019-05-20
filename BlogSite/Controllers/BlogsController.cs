be1bf9e08c375c87a24df87de70eeaedf75d89bd
Student <example@example.com>
2019-05-20 15:02:51 -0700

    add blogs-new view and begin blogs controller

--- BlogSite/Controllers/BlogsController.cs
+++ BlogSite/Controllers/BlogsController.cs
@@ -16,5 +16,25 @@ namespace BlogSite.Controllers
           List<Blog> allBlogs = Blog.GetAll();
           return View(allBlogs);
       }
+
+      [HttpGet("/blogs/new")]
+      public ActionResult New()
+      {
+        return View();
+      }
+
+      [HttpPost("/blogs/create")]
+      public ActionResult Create(string username, string password1, string password2)
+      {
+        if(password1 == password2)
+        {
+          Blog newBlog = new Blog(username, password1);
+          return RedirectToAction("Edit", newBlog);
+        }
+        else
+        {
+          return View("New");
+        }
+      }
   }
 }
