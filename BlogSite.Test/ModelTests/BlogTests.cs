2d2a2adcdbb5b6c4f1b63f6d509828d047e60606
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-05-22 14:23:24 -0700

    Add tests

--- BlogSite.Test/ModelTests/BlogTests.cs
+++ BlogSite.Test/ModelTests/BlogTests.cs
@@ -7,8 +7,14 @@ using MySql.Data.MySqlClient;
 namespace BlogSite.Tests
 {
   [TestClass]
-  public class BlogTest
+  public class BlogTest : IDisposable
   {
+    public void Dispose()
+    {
+      Blog.ClearAll();
+      Post.ClearAll();
+      Community.ClearAll();
+    }
     public BlogTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=blog_site_test;";
@@ -22,43 +28,150 @@ namespace BlogSite.Tests
     [TestMethod]
     public void GetUserName_GetsNameOfBlogUser_String()
     {
-      string username =
+      string username = "username";
       Blog newBlog = new Blog(username, "");
-      Assert.AreEqual(username, newBlog.GetUserName());
+      Assert.AreEqual(username, newBlog.GetUsername());
     }
    [TestMethod]
    public void GetPassword()
    {
-
+     string password = "password";
+     Blog newBlog = new Blog ("", password);
+     Assert.AreEqual(password, newBlog.GetPassword());
    }
    [TestMethod]
    public void GetTitle()
    {
-
-   }
-   [TestMethod]
-   public void SetTitle()
-   {
-
+     string title = "title";
+     Blog newBlog = new Blog ("", "");
+     newBlog.SetTitle(title);
+     Assert.AreEqual(title, newBlog.GetTitle());
    }
    [TestMethod]
    public void GetId()
    {
-
+     Blog newBlog = new Blog ("", "");
+     Assert.AreEqual(0, newBlog.GetId());
    }
    [TestMethod]
    public void GetAbout()
    {
+     string about = "about";
+     Blog newBlog = new Blog ("", "");
+     newBlog.SetAbout(about);
+     Assert.AreEqual(about, newBlog.GetAbout());
+   }
 
+   [TestMethod]
+   public void GetAll_ReturnsEmptyBlogList_BlogList()
+   {
+     List<Blog> allBlogs = new List <Blog>{};
+     List <Blog> result = Blog.GetAll();
+     CollectionAssert.AreEqual(allBlogs, result);
    }
    [TestMethod]
-   public void SetAbout()
+   public void Save_SavesBlogToDatabase_BlogList()
    {
-
+     Blog newBlog = new Blog("", "");
+     newBlog.Save();
+     List <Blog> result = Blog.GetAll();
+     List<Blog> testList = new List <Blog>{newBlog};
+     CollectionAssert.AreEqual(result, testList);
+   }
+   [TestMethod]
+   public void FindById_FindsBlogFromDatabase_Blog()
+   {
+     Blog newBlog = new Blog("", "");
+     newBlog.Save();
+     Blog foundBlog = Blog.FindById(newBlog.GetId());
+     Assert.AreEqual(newBlog, foundBlog);
+   }
+   [TestMethod]
+   public void FindByUsername_FindBlogFromDatabaseBlog()
+   {
+     Blog newBlog = new Blog("", "");
+     newBlog.Save();
+     Blog foundBlog = Blog.FindByUsername(newBlog.GetUsername());
+     Assert.AreEqual(newBlog, foundBlog);
+   }
+   [TestMethod]
+   public void Delete_DeletesSpecificBlog_True()
+   {
+     Blog newBlog = new Blog("", "");
+     newBlog.Save();
+     Blog newBlog2 = new Blog("", "");
+     newBlog2.Save();
+     newBlog.Delete();
+     List<Blog> result = Blog.GetAll();
+     Assert.AreEqual(newBlog2, result[0]);
+   }
+   [TestMethod]
+   public void Edit_ChangesBlogElements_Strings()
+   {
+     Blog newBlog = new Blog("", "");
+     newBlog.Save();
+     string password = "password";
+     string about = "about";
+     newBlog.Edit("", password, "", about);
+     newBlog.Save();
+     Assert.AreEqual(password, newBlog.GetPassword());
+     Assert.AreEqual(about, newBlog.GetAbout());
+   }
+   [TestMethod]
+   public void Authenticate_ChecksIfPasswordAndUsernameAreTheSame_True()
+   {
+     Blog newBlog = new Blog("a", "b");
+     newBlog.Save();
+     Assert.AreEqual(true, Blog.Authenticate("a", "b"));
+   }
+   [TestMethod]
+   public void Login_AddsUserToSessionBlogTable_User()
+   {
+     Blog newBlog = new Blog("a", "b", 2);
+     newBlog.Save();
+     Blog.Login("a", "b");
+     Assert.AreEqual(newBlog.GetId(), SessionBlog.GetId());
+   }
+   [TestMethod]
+   public void GetPosts_GetsAllBlogPosts_PostList()
+   {
+     Blog newBlog = new Blog("a", "b");
+     newBlog.Save();
+     Post newPost = new Post("", "", "", 5);
+     newPost.Save();
+     Post newPost2 = new Post("", "", "", newBlog.GetId());
+     newPost2.Save();
+     List <Post> result = newBlog.GetPosts();
+     List <Post> postList = new List <Post>{newPost2};
+     CollectionAssert.AreEqual(result, postList);
    }
 
+   [TestMethod]
+   public void GetCommunities_ReturnsBlogCommunities_CommunityList()
+   {
+     Community newCommunity = new Community("","");
+     newCommunity.Save();
+     Blog newBlog = new Blog("","");
+     newBlog.Save();
+     newBlog.AddCommunity(newCommunity.GetId());
+     List <Community> result = newBlog.GetCommunities();
+     List <Community> communityList = new List <Community>{newCommunity};
+     Console.WriteLine(result.Count);
+     Console.WriteLine(communityList.Count);
+     CollectionAssert.AreEqual(result, communityList);
+   }
 
-
+   // [TestMethod]
+   // public void AddCommunity()
+   // {
+   //
+   // }
+   //
+   // [TestMethod]
+   // public void RemoveCommunity()
+   // {
+   //
+   // }
 
  }
 }
--- BlogSite.Test/ModelTests/PostTests.cs
+++ BlogSite.Test/ModelTests/PostTests.cs
@@ -103,6 +103,32 @@ namespace BlogSite.Tests
       Assert.AreEqual(newPost2, result[0]);
     }
 
+    [TestMethod]
+    public void Edit_EditsPostElements_Strings()
+    {
+      Post newPost = new Post("", "", "", 0);
+      newPost.Save();
+      string title = "title";
+      string content = "content";
+      string file = "file";
+      newPost.Edit(title, content, file);
+      Assert.AreEqual(title, newPost.GetTitle());
+      Assert.AreEqual(content, newPost.GetContent());
+    }
+    [TestMethod]
+    public void PostSearch_SearchesForContentContainingSearchWord_Content()
+    {
+      Post newPost = new Post("", "", "", 0);
+      newPost.Save();
+      Post newPost2 = new Post("", "a", "", 1);
+      newPost2.Save();
+      List<Post> newList = new List <Post>{newPost2};
+      List <Post> result = Post.PostSearch("a");
+      Console.WriteLine(result.Count);
+      Console.WriteLine(newList.Count);
+      CollectionAssert.AreEqual(result, newList);
+    }
+
    }
 
 }
