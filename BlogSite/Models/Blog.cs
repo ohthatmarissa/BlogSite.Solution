d3346b5b622bb27520fc2d16fdbda5bec3119fec
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-05-22 09:18:30 -0700

    Add fix for  blog view

--- BlogSite/Models/Blog.cs
+++ BlogSite/Models/Blog.cs
@@ -317,7 +317,7 @@ namespace BlogSite.Models
           string postTitle = rdr.GetString(2);
           string postContent = rdr.GetString(3);
           DateTime postDate = rdr.GetDateTime(4);
-          string postFile = rdr.GetString(5);
+          string postFile =  rdr.IsDBNull(5) ? null : rdr.GetString(5);
 
           Post thisPost = new Post(postTitle, postContent, postFile, blogId, postId);
           thisPost.SetDate(postDate);
